27ec90511513bf7ade81780f812bd410f0ab1f37
Kateryna <kvkyrpa@gmail.com>
2023-10-25 19:47:43 +0200

    feat: task complete

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var cors = require('cors');
 require('dotenv').config()
+const multer = require('multer')
+const upload = multer({ dest: 'uploads/' })
 
 var app = express();
 
@@ -11,7 +13,16 @@ app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res) => {
+  const name = req.file.originalname;
+  const type = req.file.mimetype;
+  const size = req.file.size;
+  res.json({
+    "name": name,
+    "type": type,
+    "size": size
+  })
+});
 
 
 const port = process.env.PORT || 3000;
